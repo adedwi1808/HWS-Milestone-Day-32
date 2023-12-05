496a2c3d2a483964aa2a0251a9b08a5d48bb5463
Ade Dwi Prayitno <adedwip1808@email.com>
2023-12-05 21:40:25 +0700

    FINAL

--- HWS-Milestone-Day-32/ViewController.swift
+++ HWS-Milestone-Day-32/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var shoppingList: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Milestone - 32"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .plain, target: self, action: #selector(resetShoppingList))
+        
+        let addButton = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(showInputPopup))
+        let share = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(share))
+        navigationItem.rightBarButtonItems = [addButton, share]
     }
 
-
+    @objc func showInputPopup() {
+        let ac = UIAlertController(title: "Add Item", message: "Please Type Item Name", preferredStyle: .alert)
+        
+        ac.addTextField()
+        
+        let action = UIAlertAction(title: "Add", style: .default ) { [weak self, weak ac] _ in
+            guard let self,
+                  let ac else { return }
+            guard let text = ac.textFields?.first?.text else { return }
+                    
+            self.insert(itemName: text)
+        }
+        
+        ac.addAction(action)
+        present(ac, animated: true)
+    }
+    
+    @objc func share() {
+        let shoppingListTextJoined = shoppingList.joined(separator: "\n")
+        let vc = UIActivityViewController(activityItems: [shoppingListTextJoined], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItems![1]
+        present(vc, animated: true)
+    }
+    
+    
+    func insert(itemName: String) {
+        shoppingList.insert(itemName, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    @objc func resetShoppingList() {
+        shoppingList.removeAll()
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        
+        return cell
+    }
 }
 
