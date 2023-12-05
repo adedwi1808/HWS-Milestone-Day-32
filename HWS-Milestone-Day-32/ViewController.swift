1b566b3f25f7fef6d0c5d33f0a489fbca33ad4f9
Ade Dwi Prayitno <adedwip1808@email.com>
2023-12-05 21:13:09 +0700

    Initial Commit

---
+++ HWS-Milestone-Day-32/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWS-Milestone-Day-32
+//
+//  Created by Ade Dwi Prayitno on 05/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
